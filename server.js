cadc486f0caf6d1e0404c34117e1206cefe247ae
carpenter-js <carpenterbarrett@gmail.com>
2018-12-07 12:47:24 -0700

    travis push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const passport = require('passport');
+const passport = require('passport'); 
 
 // Security
 const localStrategy = require('./passport/local');
